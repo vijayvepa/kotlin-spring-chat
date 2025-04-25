94b25e1ebbee1194e3e3e744812ec92475a383b3
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-04-25 12:50:49 -0400

    db schema

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+	runtimeOnly("com.h2database:h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
