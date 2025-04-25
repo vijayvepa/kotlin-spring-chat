1a2538a08d5c71617d4629839b08d3aadad31423
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-04-25 16:18:33 -0400

    in memory h2 test

--- src/test/kotlin/com/example/kotlin/chat/ChatKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/kotlin/chat/ChatKotlinApplicationTests.kt
@@ -1,13 +1,102 @@
 package com.example.kotlin.chat
 
+import com.example.kotlin.chat.repository.ContentType
+import com.example.kotlin.chat.repository.Message
+import com.example.kotlin.chat.repository.MessageRepository
+import com.example.kotlin.chat.service.MessageVM
+import com.example.kotlin.chat.service.UserVM
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpMethod
+import org.springframework.http.RequestEntity
+import java.net.URI
+import java.net.URL
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = [
+	"spring.datasource.url=jdbc:h2:mem:testdb"
+])
 class ChatKotlinApplicationTests {
+
+	@Autowired
+	lateinit var client: TestRestTemplate
+
+	@Autowired
+	lateinit var  messageRepository: MessageRepository
+
+	lateinit var lastMessageId: String
+
+	val now : Instant = Instant.now()
+
+
+
 
 	@Test
 	fun contextLoads() {
+		println("lastMessage: ${lastMessageId}")
 	}
+
+	@BeforeEach
+	fun setup() {
+		val secondBeforeNow = now.minusSeconds(1)
+		val twoSecondsBeforeNow = now.minusSeconds(2)
+
+		val savedMessages = messageRepository.saveAll(listOf(
+			Message("testMessage1", ContentType.PLAIN, twoSecondsBeforeNow, "test", "http://test.com"),
+			Message("testMessage2", ContentType.PLAIN, secondBeforeNow, "test", "http://test.com"),
+		))
+
+		lastMessageId = savedMessages.first().id ?: ""
+
+	}
+
+	@AfterEach
+	fun teardown() {
+		messageRepository.deleteAll()
+	}
+
+
+	@ParameterizedTest
+	@ValueSource(booleans = [true, false])
+	fun `test that messages API returns latest messages` (withLastMessageId: Boolean) {
+		val messages : List<MessageVM>? = client.exchange(
+			RequestEntity<Any> (
+				HttpMethod.GET,
+				URI("/api/v1/messages?lastMessageId=${if(withLastMessageId) lastMessageId else ""}"),
+			),
+			object : ParameterizedTypeReference<List<MessageVM>>() {}
+		).body
+
+		if(!withLastMessageId) {
+			assertThat(messages?.map { with(it){ copy(id=null, sent = sent.truncatedTo(ChronoUnit.MILLIS))} })
+				.first().isEqualTo(MessageVM(
+                    "testMessage1",
+                    UserVM(
+                        "test", URL("http://test.com")
+
+                    ),
+					now.minusSeconds(2).truncatedTo(ChronoUnit.MILLIS)
+                ))
+		}
+
+		assertThat ( messages?.map { with(it){copy(id=null, sent = sent.truncatedTo(ChronoUnit.MILLIS)) } })
+			.containsSubsequence(MessageVM(
+                    content = "testMessage2",
+                    user = UserVM("test", URL("http://test.com")),
+                    sent = now.minusSeconds(1).truncatedTo(ChronoUnit.MILLIS)
+
+                ),
+			)
+	}
+
 
 }
