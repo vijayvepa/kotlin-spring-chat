18a023ccaf8a38f0a50da09dfea4a643f322f783
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-04-25 13:35:19 -0400

    persistent chatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 
 	runtimeOnly("com.h2database:h2")
 
