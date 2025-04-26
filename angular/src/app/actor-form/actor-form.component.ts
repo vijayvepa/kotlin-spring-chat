942ee302c293f710f07512852c438d55807e1d65
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-04-26 14:44:04 -0400

    angullar forms

---
+++ angular/src/app/actor-form/actor-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+	selector: 'app-actor-form',
+	imports: [FormsModule, JsonPipe],
+	templateUrl: './actor-form.component.html',
+	styleUrl: './actor-form.component.css'
+})
+export class ActorFormComponent implements OnInit {
+
+	skills = ['Acting', 'Singing', 'Dancing', 'Directing', 'Writing'];
+	model = new Actor(1, 'John Doe', this.skills[0], 'Hollywood');
+	submitted = false;
+
+
+	ngOnInit(): void {
+		const myActress = new Actor(1, 'John Doe', this.skills[0], 'Hollywood');
+		console.log('My actress:', myActress);
+	}
+	
+
+	onSubmit() {
+		this.submitted = true;
+	}
+
+	
+
+}
+
+export class Actor {
+	constructor(
+		public id: number,
+		public name: string,
+		public skill: string,
+		public studio: string
+	) { }
+}
--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { ActorFormComponent } from './actor-form/actor-form.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [FormsModule, JsonPipe, ActorFormComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent{
+
+	title = 'angular';
+	messages: string[] = ["Nothing", "at", "all"]
+	newMessage: string | undefined = undefined ;
+
+	addMessage() {
+		this.messages.push(this.newMessage!);
+	}
+	
+	clearMessages() {
+		this.messages = [];
+	}
 }
+
